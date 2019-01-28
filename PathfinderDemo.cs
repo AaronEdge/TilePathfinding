b6b15a5163c3bfaa345373067af55cfd4d032ac0
AaronEdge <8917553+AaronEdge@users.noreply.github.com>
2019-01-28 14:07:55 +0000

    Added PathfinderDemo.cs file
    Added an example of how to use the tile pathfinding script.

---
+++ PathfinderDemo.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TilePathFinder;
+
+public class PathfinderDemo : MonoBehaviour
+{
+
+    public Grid TileGrid;
+    private TilePathfinder pathFinder;
+
+    void Start()
+    {
+        pathFinder = new TilePathfinder(TileGrid, "Blocking", "Floor");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int endPoint = TileObject.WorldToCell(mousePos);
+            Vector3Int startPoint = TileObject.WorldToCell(transform.position);
+
+            List<Vector3Int> path = pathFinder.FindPath(startPoint, endPoint);
+            playerMover.Path = path;
+
+            int steps = 0;
+            foreach (Vector3Int step in path)
+            {
+                steps++;
+                Debug.Log("step " + steps + ": (" + step.x + "," + step.y + ")");
+                transform.position = step;
+            }
+        }
+    }
+}
